94f6035124079a2dff408622e2cc2be8b566e6c6
Samriddhi Chauhan <samriddhichauhan27@gmail.com>
2026-07-24 04:00:13 +0530

    Added docker-compose.yml file etc.

--- src/main/java/com/dreamsync/config/OpenApiConfig.java
+++ src/main/java/com/dreamsync/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.dreamsync.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI dreamSyncOpenAPI() {
 
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("DreamSync API")
@@ -20,8 +25,18 @@ public class OpenApiConfig {
                         .description("A Spring Boot REST API for project management, dependency analysis, impact analysis, and dashboard analytics.")
                         .contact(new Contact()
                                 .name("Samriddhi Chauhan")
-                                .email("samriddhichauhan27@gamil.com"))
+                                .email("samriddhichauhan27@gmail.com"))
                         .license(new License()
-                                .name("MIT License")));
+                                .name("MIT License")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
     }
 }
